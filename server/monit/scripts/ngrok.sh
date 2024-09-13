e18a0bd616da59468a1b924c8fca87d7e692d27d
Kuzmin_D <z644813828@gmail.com>
2024-09-13 12:27:04 +0300

    upd scripts

--- server/monit/scripts/ngrok.sh
+++ server/monit/scripts/ngrok.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-NGROK_RESTART_CMD="/opt/ngrok start -all > /dev/null &"
+NGROK_RESTART_CMD="/opt/ngrok start --all > /dev/null &"
 NGROK_STATUS_FILE="localhost:4040/api/tunnels"
 PARSE_CMD_SSH="jq -r '.tunnels[]|select(.name==\"server_ssh\")|.public_url'"
 PARSE_CMD_WEB="jq -r '.tunnels[]|select(.name==\"asus_router\")|.public_url'"
@@ -17,12 +17,12 @@ fi
 CMD="curl -s $NGROK_STATUS_FILE | $PARSE_CMD_SSH"
 DATA=$(eval $CMD)
 DATA=$(echo $DATA | awk '{ print substr ($0, 7 ) }')
+if [ -z "${DATA}" ]; then exit 1; fi
 echo $DATA | awk -F ":" '{print "ssh -p "$2 " dmitriy@"$1}'
 
 CMD="curl -s $NGROK_STATUS_FILE | $PARSE_CMD_WEB"
 DATA=$(eval $CMD)
+if [ -z "${DATA}" ]; then exit 1; fi
 echo $DATA
 
 exit 0;
-
-
